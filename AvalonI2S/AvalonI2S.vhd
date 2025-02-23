d1f430f64974b7a20c0db1c292427e214bbbcd81
Zork Zorkus <zorkzorkus@gmail.com>
2025-02-23 23:04:34 +0100

    AvalonI2S Fix: remove readdatavalid signal and refactor

--- AvalonI2S/AvalonI2S.vhd
+++ AvalonI2S/AvalonI2S.vhd
@@ -21,7 +21,6 @@ entity AvalonI2S is
 		avalon_write         : in std_logic;
 		avalon_read          : in std_logic;
 		avalon_readdata      : out std_logic_vector(31 downto 0);
-		avalon_readdatavalid : out std_logic;
 		avalon_waitrequest   : out std_logic;
 		avalon_irq           : out std_logic;
 
@@ -72,88 +71,96 @@ architecture rtl of AvalonI2S is
 
 
 	-- RISC-V core / avalon bus signals, exclusively in fastclk domain
-	signal out_occupancy : std_logic_vector(BUFFER_BITLENGTH-1 downto 0);
-	signal in_occupancy : std_logic_vector(BUFFER_BITLENGTH-1 downto 0);
-	signal reg_out_mark : std_logic_vector(BUFFER_BITLENGTH-1 downto 0) := std_logic_vector(to_unsigned(2**(BUFFER_BITLENGTH-1), BUFFER_BITLENGTH));
-	signal reg_in_mark : std_logic_vector(BUFFER_BITLENGTH-1 downto 0) := std_logic_vector(to_unsigned(2**(BUFFER_BITLENGTH-1), BUFFER_BITLENGTH));
-	signal reg_status : std_logic_vector(4 downto 0) := "00000";
-	signal reg_writecounter : std_logic_vector(31 downto 0) := x"00000000";
-	signal ava_readvalid : std_logic := '0';
+	signal sa_out_fill     : std_logic_vector(BUFFER_BITLENGTH-1 downto 0);
+	signal sa_in_fill      : std_logic_vector(BUFFER_BITLENGTH-1 downto 0);
+
+	signal ra_out_mark     : std_logic_vector(BUFFER_BITLENGTH-1 downto 0) := std_logic_vector(to_unsigned(2**(BUFFER_BITLENGTH-1), BUFFER_BITLENGTH));
+	signal ra_in_mark      : std_logic_vector(BUFFER_BITLENGTH-1 downto 0) := std_logic_vector(to_unsigned(2**(BUFFER_BITLENGTH-1), BUFFER_BITLENGTH));
+	signal ra_status       : std_logic_vector(4 downto 0) := "00000";
+	signal ra_writecounter : std_logic_vector(31 downto 0) := x"00000000";
+	signal ra_waitrequest  : std_logic := '0';
 
 	-- I2S signals, exclusively in slowclk domain
-	signal out_word : std_logic_vector(31 downto 0);
-	signal in_word : std_logic_vector(31 downto 0);
-	signal bitcounter : integer range 0 to 31;
-	signal prescaler : integer range 0 to 7 := 0;
-	signal wclk : std_logic := '0';
+	signal rs_out_word : std_logic_vector(31 downto 0);
+	signal rs_in_word : std_logic_vector(31 downto 0);
+	signal rs_bitcounter : integer range 0 to 31;
+	signal rs_prescaler : integer range 0 to 7 := 0;
+	signal rs_wclk : std_logic := '0';
 
 begin
 
-	process (clk_sys) begin
-		if (rising_edge(clk_sys)) then
+	process (clk_sys, resetn) begin
+		if (resetn = '0') then
+			ra_out_mark <= std_logic_vector(to_unsigned(2**(BUFFER_BITLENGTH-1), BUFFER_BITLENGTH));
+			ra_in_mark <= std_logic_vector(to_unsigned(2**(BUFFER_BITLENGTH-1), BUFFER_BITLENGTH));
+			ra_status(0) <= '0';
+			ra_status(2) <= '0';
+			ra_status(4) <= '0';
+			ra_writecounter <= x"00000000";
+			ra_waitrequest <= '0';
+		elsif (rising_edge(clk_sys)) then
 
-			if (avalon_write = '1') then
+			ra_waitrequest <= '1';
+
+			if (avalon_write = '1' and ra_waitrequest = '1') then
+
+				ra_waitrequest <= '0';
 
 				case avalon_address is
 					when "000" =>
-						reg_status(0) <= avalon_writedata(0);
-						reg_status(2) <= avalon_writedata(2);
-						reg_status(4) <= avalon_writedata(4);
-					when "011" => reg_out_mark <= avalon_writedata(BUFFER_BITLENGTH-1 downto 0);
-					when "100" => reg_in_mark <= avalon_writedata(BUFFER_BITLENGTH-1 downto 0);
+						ra_status(0) <= avalon_writedata(0);
+						ra_status(2) <= avalon_writedata(2);
+						ra_status(4) <= avalon_writedata(4);
+					when "011" => ra_out_mark <= avalon_writedata(BUFFER_BITLENGTH-1 downto 0);
+					when "100" => ra_in_mark <= avalon_writedata(BUFFER_BITLENGTH-1 downto 0);
+					when "111" =>
+						if not (out1_f_w + 1 = out2_f_r) then
+							out1_f_w <= out1_f_w + 1;
+							ra_writecounter <= std_logic_vector(unsigned(ra_writecounter) + 1);
+							out_fifo(out1_f_w) <= avalon_writedata;
+						else
+							-- Buffer is full, assert waitsignal
+								ra_waitrequest <= '1';
+						end if;
+					when others => null;
+				end case;
+
+			elsif (avalon_read = '1' and ra_waitrequest = '1') then
+
+				ra_waitrequest <= '0';
+				avalon_readdata <= (others => '0');
+
+				case avalon_address is
+					when "000" => avalon_readdata(4 downto 0) <= ra_status;
+					when "001" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= sa_out_fill;
+					when "010" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= sa_in_fill;
+					when "011" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= ra_out_mark;
+					when "100" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= ra_in_mark;
+					when "101" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= std_logic_vector(to_unsigned((2**BUFFER_BITLENGTH)-1, BUFFER_BITLENGTH));
+					when "110" => avalon_readdata <= ra_writecounter;
+					when "111" =>
+						if (ra_status(4) = '0') then
+							-- core disabled, return 0 (default)
+						elsif not (in2_f_w = in1_f_r) then
+							avalon_readdata <= in_fifo(in1_f_r);
+							in1_f_r <= in1_f_r + 1;
+						else
+							-- core enabled, but no data available, wait
+							ra_waitrequest <= '1';
+						end if;
 					when others => null;
 				end case;
 
 				if (avalon_address = "111") then
-					-- Write to ring buffer if there is space left
-					if not (out1_f_w + 1 = out2_f_r) then
-						out1_f_w <= out1_f_w + 1;
-						reg_writecounter <= std_logic_vector(unsigned(reg_writecounter) + 1);
-						out_fifo(out1_f_w) <= avalon_writedata;
-					else
-						-- Buffer is full, assert waitsignal
-						avalon_waitrequest <= '1';
+					if not (in1_f_r = in2_f_w) then
+						avalon_readdata <= in_fifo(in1_f_r);
+						in1_f_r <= in1_f_r + 1;
 					end if;
 				end if;
 
-			else
-				avalon_waitrequest <= '0';
-			end if; -- avalon_write = '1'
-
-			if (avalon_read = '1') then
-				
-				-- If readvalid is already asserted, wait for avalon-master to deassert read signal
-				-- to finish the memory transfer, only then a new transfer can be initiated
-				if (ava_readvalid = '0') then
-					avalon_readdata <= (others => '0');
-					case avalon_address is
-						when "000" => avalon_readdata(4 downto 0) <= reg_status;
-						when "001" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= out_occupancy;
-						when "010" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= in_occupancy;
-						when "011" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= reg_out_mark;
-						when "100" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= reg_in_mark;
-						when "101" => avalon_readdata(BUFFER_BITLENGTH-1 downto 0) <= std_logic_vector(to_unsigned((2**BUFFER_BITLENGTH)-1, BUFFER_BITLENGTH));
-						when "110" => avalon_readdata <= reg_writecounter;
-						when others => null;
-					end case;
-
-					if (avalon_address = "111") then
-						if not (in1_f_r = in2_f_w) then
-							avalon_readdata <= in_fifo(in1_f_r);
-							in1_f_r <= in1_f_r + 1;
-							ava_readvalid <= '1';
-						end if;
-					else
-						ava_readvalid <= '1';
-					end if;
-				end if;
-
-			else 
-				ava_readvalid <= '0';
 			end if;
 
 		end if;
-
 	end process;
 
 	-- Metastability Synchronization Chain
@@ -169,7 +176,7 @@ begin
 			out1_s_w <= out1_m_w;
 			in1_m_r <= in1_f_r;
 			in1_s_r <= in1_m_r;
-			cdc1_m_enable <= reg_status(4);
+			cdc1_m_enable <= ra_status(4);
 			cdc1_s_enable <= cdc1_m_enable;
 		end if;
 	end process;
@@ -178,34 +185,34 @@ begin
 	process (clk_i2s) begin
 		if (rising_edge(clk_i2s) and cdc1_s_enable = '1') then
 
-			prescaler <= prescaler + 1;
+			rs_prescaler <= rs_prescaler + 1;
 
-			if (prescaler = 0) then
+			if (rs_prescaler = 0) then
 
 				i2s_sclk <= '1';
-				bitcounter <= bitcounter + 1;
-				i2s_dato <= out_word(31);
-				out_word(31 downto 1) <= out_word(30 downto 0);
-				in_word(0) <= i2s_dati;
-				in_word(31 downto 1) <= in_word(30 downto 0);
+				rs_bitcounter <= rs_bitcounter + 1;
+				i2s_dato <= rs_out_word(31);
+				rs_out_word(31 downto 1) <= rs_out_word(30 downto 0);
+				rs_in_word(0) <= i2s_dati;
+				rs_in_word(31 downto 1) <= rs_in_word(30 downto 0);
 
 				-- Try to get new word from ring buffer when shifting out last bit of previous word
 				-- When there is no word available shift previous word again
-				if (bitcounter = 0) then
+				if (rs_bitcounter = 0) then
 					if not (out1_s_w = out2_s_r) then
 						out2_s_r <= out2_s_r + 1;
-						out_word <= out_fifo(out2_s_r);
+						rs_out_word <= out_fifo(out2_s_r);
 					end if;
 					if not (in2_s_w + 1 = in1_s_r) then
-						in_fifo(in2_s_w) <= in_word;
+						in_fifo(in2_s_w) <= rs_in_word;
 						in2_s_w <= in2_s_w + 1;
 					end if;
-					wclk <= not wclk;
-				elsif (bitcounter = 16) then
-					wclk <= not wclk;
+					rs_wclk <= not rs_wclk;
+				elsif (rs_bitcounter = 16) then
+					rs_wclk <= not rs_wclk;
 				end if;
 
-			elsif (prescaler = 4) then
+			elsif (rs_prescaler = 4) then
 				i2s_sclk <= '0';
 			end if;
 
@@ -213,15 +220,15 @@ begin
 	end process;
 
 	-- combinatorial signals
-	reg_status(1) <= '1' when unsigned(out_occupancy) <= unsigned(reg_out_mark) else '0';
-	reg_status(3) <= '1' when unsigned(in_occupancy) >= unsigned(reg_in_mark) else '0';
-	out_occupancy <= std_logic_vector(to_unsigned(out1_f_w - out2_f_r, out_occupancy'length));
-	in_occupancy <= std_logic_vector(to_unsigned(in2_f_w - in1_f_r, in_occupancy'length));
-	avalon_irq <= '1' when (reg_status(0)  = '1' and reg_status(1) = '1') or (reg_status(2) = '1' and reg_status(3) = '1') else '0';	
-	avalon_readdatavalid <= ava_readvalid;
+	ra_status(1) <= '1' when unsigned(sa_out_fill) <= unsigned(ra_out_mark) else '0';
+	ra_status(3) <= '1' when unsigned(sa_in_fill) >= unsigned(ra_in_mark) else '0';
+	sa_out_fill <= std_logic_vector(to_unsigned(out1_f_w - out2_f_r, sa_out_fill'length));
+	sa_in_fill <= std_logic_vector(to_unsigned(in2_f_w - in1_f_r, sa_in_fill'length));
+	avalon_irq <= '1' when (ra_status(0)  = '1' and ra_status(1) = '1') or (ra_status(2) = '1' and ra_status(3) = '1') else '0';
 
 	-- port signals
-	i2s_wclk <= wclk;
+	avalon_waitrequest <= ra_waitrequest;
+	i2s_wclk <= rs_wclk;
 	i2s_mclk <= clk_i2s when cdc1_s_enable = '1' else '0';
 
 end architecture;
